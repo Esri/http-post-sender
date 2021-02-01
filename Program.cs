17ec9cd6acb6ea15eb4554d9dbc85ab02e9f0f60
Ken Gorton <kgorton@esri.com>
2021-02-01 12:37:52 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ using System.Configuration;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -39,8 +40,9 @@ namespace HttpPostSender
         private static string username = ConfigurationManager.AppSettings["username"];
         private static string password = ConfigurationManager.AppSettings["password"];
         private static string fileUrl = ConfigurationManager.AppSettings["fileUrl"];
-        private static bool hasHeaders = Boolean.Parse(ConfigurationManager.AppSettings["hasHeaders"]);
+        private static bool hasHeaderRow = Boolean.Parse(ConfigurationManager.AppSettings["hasHeaderRow"]);
         private static string fieldDelimiter = ConfigurationManager.AppSettings["fieldDelimiter"];
+        private static bool convertToJson = Boolean.Parse(ConfigurationManager.AppSettings["convertToJson"]);
         private static int numLinesPerBatch = Int32.Parse(ConfigurationManager.AppSettings["numLinesPerBatch"]);
         private static int sendInterval = Int32.Parse(ConfigurationManager.AppSettings["sendInterval"]);
         private static int timeField = Int32.Parse(ConfigurationManager.AppSettings["timeField"]);
@@ -77,32 +79,37 @@ namespace HttpPostSender
 
                 readStream.Close();
 
-                int c = contentArray.Length;
+                //int c = contentArray.Length;
                 bool runTask = true;
 
 
-    
-                if ((headerLine = contentArray[0]) != null)
-                {
-                    //schema = new JObject();
-                    fields = headerLine.Split(fieldDelimiter);
-                    int fieldNum = 1;
-                    foreach (string fieldName in fields)
+                if(convertToJson){
+                    if ((headerLine = contentArray[0]) != null)
                     {
-                        if (hasHeaders){
-                            schema[fieldName] = null;
+                        //schema = new JObject();
+                        fields = headerLine.Split(fieldDelimiter);
+                        int fieldNum = 1;
+                        foreach (string fieldName in fields)
+                        {
+                            if (hasHeaderRow){
+                                schema[fieldName] = null;
+                            }
+                            else{  
+                                string genericFieldName = $"field{fieldNum}";                              
+                                schema[genericFieldName] = null;
+                            }
+                            fieldNum += 1;
                         }
-                        else{  
-                            string genericFieldName = $"field{fieldNum}";                              
-                            schema[genericFieldName] = null;
-                        }
-                        fieldNum += 1;
+                        Console.WriteLine("Schema created based on the incoming data:");
+                        Console.WriteLine(schema);
+                        Dictionary<string,string> dictObj = schema.ToObject<Dictionary<string,string>>();
+                        dictObj.Keys.CopyTo(fields,0);
                     }
-                    Console.WriteLine("Schema created based on the incoming data:");
-                    Console.WriteLine(schema);
-                    Dictionary<string,string> dictObj = schema.ToObject<Dictionary<string,string>>();
-                    dictObj.Keys.CopyTo(fields,0);
                 }
+                if (hasHeaderRow){
+                    contentArray = contentArray.Where((source, index) => index != 0).ToArray();
+                }
+                int c = contentArray.Length;
 
                 client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "*/*");
                 client.DefaultRequestHeaders.TryAddWithoutValidation("Referer", "http://localhost:8888");
@@ -122,63 +129,100 @@ namespace HttpPostSender
                 
                 int count = 0;
                 int countTotal = 0;
-                JArray eventBatch = null; 
+                //JArray eventBatch = null; 
+                dynamic[] messageBatch = null;
                 
                 var stopwatch = new Stopwatch();
+                var taskStopwatch = new Stopwatch();
                 while (runTask)
                 {
-                    for (int l = (hasHeaders ? 1 : 0); l < c; l++)
+                    taskStopwatch.Start();
+                    for (int l = 0; l < c; l++)
                     {
+                        //int lineNum = hasHeaderRow ? l+1 : l;
                         line = contentArray[l];
+                        if (String.IsNullOrEmpty(line)){
+                            continue;
+                        }
+                        //Console.WriteLine($"Line: {line}");
 
                         // Create a batch of events if needed
-                        if (eventBatch == null) 
+                        if (messageBatch == null) 
                         {
                             
-                            eventBatch = new JArray();
+                            //eventBatch = new JArray();
+                            messageBatch = new dynamic[numLinesPerBatch];
                             stopwatch.Start();
                         }
-                        eventBatch = eventBatch ?? new JArray(); 
+                        //eventBatch = eventBatch ?? new JArray(); 
+                        messageBatch = messageBatch ?? new dynamic[numLinesPerBatch];
+                        
                         dynamic[] values = line.Split(fieldDelimiter);
-                        for (int i = 0; i < schema.Count; i++)
-                        { 
-                            long longVal = 0;
-                            decimal decVal = 0;
-                            bool isLong = long.TryParse(values[i], out longVal);
-                            bool isDec = decimal.TryParse(values[i], out decVal);
-                            schema[fields[i]] = isLong ? longVal : isDec ? decVal : values[i];
-                        }
+                        
                         if (setToCurrentTime)
                         {
                             if (String.IsNullOrEmpty(dateFormat))
                             {
-                                schema[fields[timeField]] = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+                                //Console.WriteLine("setting time value");
+                                string dt = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds().ToString();
+                                values[timeField] = dt;
+                                //Console.WriteLine("done setting time value");
                             }
                             else
                             {
                                 try{
-                                    schema[fields[timeField]] = DateTime.Now.ToString(dateFormat,dateCulture);
+                                    string dt = DateTime.Now.ToString(dateFormat,dateCulture);
+                                    values[timeField] = dt;
                                 }
                                 catch(Exception e){
-                                    schema[fields[timeField]] = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+                                    string dt = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds().ToString();
+                                    values[timeField] = dt;
                                 }
                             }
                         }
+
+                        
+
+                        if(convertToJson){
+                            for (int i = 0; i < schema.Count; i++)
+                            { 
+                                long longVal = 0;
+                                decimal decVal = 0;
+                                bool isLong = long.TryParse(values[i], out longVal);
+                                bool isDec = decimal.TryParse(values[i], out decVal);
+                                schema[fields[i]] = isLong ? longVal : isDec ? decVal : values[i];
+                            }
+                            //Console.WriteLine($"Schema: {schema}");
+                        }
+                        //else{
+                        //    Console.WriteLine($"Delimited line: {string.Join(fieldDelimiter, values)}");
+                        //}
+                        
                         
                         count++;
+                        countTotal++;
 
                         // Add events to the batch.  
-                        eventBatch.Add(schema);
+                        //eventBatch.Add(schema);
+                        int j = ((l % (1 * numLinesPerBatch)) - (l % 1)) / 1;
 
+                        //Console.WriteLine($"Line #: {l}; Index: {j}");
+                        if (convertToJson){
+                            messageBatch[j] = schema;
+                        }
+                        else{                   
+                            //string message = string.Join(fieldDelimiter, values) + "\r\n";   
+                            messageBatch[j] = string.Join(fieldDelimiter, values);// + "\r\n";
+                        }
                         
-                        if (count == numLinesPerBatch)
-                        {
-
-                           
+                        if (count == numLinesPerBatch || countTotal == c)
+                        {                           
                             // send the batch of events to the REST endpoint
-                            string payload = JsonConvert.SerializeObject(eventBatch);
-                            var content = new StringContent(payload, System.Text.Encoding.UTF8, "application/json");
-                            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                            //string payload = JsonConvert.SerializeObject(eventBatch);
+                            string payload = convertToJson ? JsonConvert.SerializeObject(messageBatch) : "\r\n"+string.Join("\r\n", messageBatch);                            
+                            //Console.WriteLine($"Payload: {payload}");
+                            var content = new StringContent(payload, System.Text.Encoding.UTF8, convertToJson ? "application/json" : "text/plain");
+                            content.Headers.ContentType = new MediaTypeHeaderValue(convertToJson ? "application/json" : "text/plain");
                             //if the request failed because the token expired, get a new one and retry the request
                             try{
                                 var response = await client.PostAsync(receiverUrl, content); 
@@ -198,25 +242,26 @@ namespace HttpPostSender
                                     responseString = await response.Content.ReadAsStringAsync();
                                     responseJson = JsonConvert.DeserializeObject(responseString);                               
                                 }
-                                countTotal += count;
+                                //countTotal += count;
                                 stopwatch.Stop();
                                 int elapsed_time = (int)stopwatch.ElapsedMilliseconds;                                
                                 if (elapsed_time < sendInterval) {
-                                    Console.WriteLine(string.Format($"A batch of {count} events has been sent. It took {elapsed_time} milliseconds. Waiting for {sendInterval - elapsed_time} milliseconds. Total sent: {countTotal}."));
+                                    Console.WriteLine(string.Format($"A batch of {count} events has been sent in {elapsed_time}ms. Waiting for {sendInterval - elapsed_time}ms. Total sent: {countTotal}. Total elapsed time: {(int)taskStopwatch.ElapsedMilliseconds}ms"));
                                     Thread.Sleep(sendInterval - elapsed_time);
                                 }
                                 else
                                 {
-                                    Console.WriteLine(string.Format($"A batch of {count} events has been sent. It took {elapsed_time} milliseconds. Total sent: {countTotal}."));
+                                    Console.WriteLine(string.Format($"A batch of {count} events has been sent in {elapsed_time}ms. Total sent: {countTotal}. Total elapsed time: {(int)taskStopwatch.ElapsedMilliseconds}ms"));
                                 }
                             }
                             catch (Exception ex){
-                                Console.WriteLine(string.Format($"A batch of {count} events was sent, but the request failed. Total sent: {countTotal}."));
+                                Console.WriteLine(string.Format($"A batch of {count} events was sent, but the request failed. Total sent: {countTotal}. Total elapsed time: {(int)taskStopwatch.ElapsedMilliseconds}ms"));
                                 Console.WriteLine(ex.Message);
                             }
                             finally{
                                 
-                                eventBatch = null;
+                                //eventBatch = null;
+                                messageBatch = null;
                                 stopwatch.Reset();
                                 count = 0;
                             }                           
@@ -226,6 +271,8 @@ namespace HttpPostSender
                     if (!repeatSimulation)
                     {
                         runTask = false;
+                        taskStopwatch.Stop(); 
+                        Console.WriteLine($"Total task duration: {(int)taskStopwatch.ElapsedMilliseconds}ms");
                     }
                 }
                 
